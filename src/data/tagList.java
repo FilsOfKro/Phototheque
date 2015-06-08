64909df7ccbbdbb80b19200d196b6b3bfd31b7f8
FilsOfKro <pantry.e1302405@etud.univ-ubs.fr>
2015-06-08 22:34:34 +0200

    test > data

---
+++ src/data/tagList.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * @author Owenn
+ *
+ */
+public class tagList {
+	private static ArrayList<String> existingTags;
+	private static HashMap<String, ArrayList<Image>> link;
+	
+	public static void createTag(String tag) {
+		existingTags.add(tag);
+	}
+	
+	public void deleteTag(String tag) {
+		this.existingTags.remove(tag);
+	}
+	
+	public static boolean exists(String tag) {
+		return existingTags.contains(tag);
+	}
+	
+	public HashMap<String, Image> getImages(String tag) {
+		HashMap<String, Image> ret = new HashMap<String, Image>();
+		Iterator tagIterator = this.existingTags.listIterator();
+
+		
+		return null;
+	}
+}
