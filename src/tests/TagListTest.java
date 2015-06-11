84d5121c2035eeaffdbad0b09ccbac00c99d877f
FilsOfKro <pantry.e1302405@etud.univ-ubs.fr>
2015-06-11 15:10:49 +0200

    Before trying HashMap<String>,ArrayList<STRING>

--- src/tests/TagListTest.java
+++ src/tests/TagListTest.java
@@ -4,8 +4,14 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import model.Image;
 import model.TagList;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -13,21 +19,36 @@ import org.junit.Test;
  *
  */
 public class TagListTest {
-
+	TagList tl = TagList.getInstance();
+	static Image im1;
+	
+	@BeforeClass
+	public static void init() throws IOException {
+		im1 = new Image("D:\\data\\im1.jpg");
+		ArrayList<String> ar = new ArrayList<String>();
+		ar.add("tag");
+		im1 = new Image("D:\\data\\im2.jpg", ar);
+	}
+	
+	
 	/**
 	 * Test method for {@link model.TagList#getInstance()}.
 	 */
 	@Test
 	public void testGetInstance() {
-		assertNotNull(TagList.getInstance());
+		
+		assertNotNull("Verify that if not instance exists, object is intanciated", tl);
+		assertEquals("Verify that if we call method again, no instance is created", tl, TagList.getInstance());
 	}
 
 	/**
 	 * Test method for {@link model.TagList#addTag(model.Image, java.lang.String)}.
+	 * @throws IOException 
 	 */
 	@Test
 	public void testAddTag() {
-		fail("Not yet implemented");
+		tl.addTag(im1, "tag1");
+		assertTrue(tl.exists("tag1"));
 	}
 
 	/**
@@ -40,10 +61,11 @@ public class TagListTest {
 
 	/**
 	 * Test method for {@link model.TagList#exists(java.lang.String)}.
+	 * @throws IOException 
 	 */
 	@Test
-	public void testExists() {
-		fail("Not yet implemented");
+	public void testExists(){
+		assertTrue(tl.exists("tag1"));
 	}
 
 	/**
